a094f0ace5f514f18c687e8b5d36aa166ec32006
Francesco <28204054+FrancescoXD@users.noreply.github.com>
2020-06-30 17:39:34 +0200

    Create variables.c

---
+++ Basics/variables.c
@@ -0,0 +1,26 @@
+// include the standard library (standard input output)
+#include <stdio.h>
+
+// the main program
+int main() {
+    short int shortNumber = 1;
+    long int longNumber = 10000;
+    int integerNumber = 10;
+    unsigned int unsignedIntegerNumber = 10;
+
+    char character = 'A';
+
+    double doubleNumber = 1.10;
+    float floatNumber = 1.10;
+    float floatCroppedNumber = 1.10;
+
+    /*
+        %d = integer
+        %c = character
+        &f = double or float
+        &.{n}f = round off to 2 decimal place
+    */
+    printf("%d %d %d %d %c %f %f %.2f", shortNumber, longNumber, integerNumber, unsignedIntegerNumber, character, doubleNumber, floatNumber, floatCroppedNumber);
+
+    return 0;
+}
