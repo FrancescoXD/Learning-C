419dd0ea732bd9bbbeeddffa6cdc4e98c75e0c12
Francesco <28204054+FrancescoXD@users.noreply.github.com>
2020-06-30 18:54:56 +0200

    Create triangles.c

---
+++ Exercises/triangles.c
@@ -0,0 +1,18 @@
+// write a program that forms a right triangle using *
+
+#include <stdio.h>
+
+int main() {
+   int n, i, j;
+
+   n = 5;
+
+   for(i = 1; i <= n; i++) {
+      for(j = 1; j <= i; j++)
+         printf("* ");
+
+      printf("\n");
+   }
+
+   return 0;
+}
