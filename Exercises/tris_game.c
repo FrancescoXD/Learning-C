b4f1b86d1f491464814270171573077876e3094d
Francesco <28204054+FrancescoXD@users.noreply.github.com>
2020-07-02 15:05:59 +0200

    Update tris_game.c

--- Exercises/tris_game.c
+++ Exercises/tris_game.c
@@ -1,12 +1,12 @@
 // write a tris game
 #include <stdio.h>
 
-enum Status { WON, LOSE, CONTINUE, P1, P2 };
+enum Status { WON, LOSE, P1, P2 };
 
 void showGameDisplay(char a, char b, char c, char d, char e, char f, char g, char h, char i);
-void insertSymbol(char position, enum Status currentPlayer);
+void insertSymbol(char position);
 enum Status checkPosition(char position);
-char askPlayerPosition(enum Status currentPlayer);
+char askPlayerPosition();
 
 enum Status currentPlayer = P1;
 
@@ -21,7 +21,6 @@ char h = ' ';
 char i = ' ';
 
 int main() {
-    enum Status gameStatus;
     enum Status player1;
     enum Status player2;
 
@@ -30,12 +29,13 @@ int main() {
 
     showGameDisplay(a, b, c, d, e, f, g, h, i);
     printf("%s", "Player 1 Symbol = X\nPlayer 2 Symbol = O\n\n");
-    askPlayerPosition(currentPlayer);
-    gameStatus = CONTINUE;
+    askPlayerPosition();
 
     do {
-        askPlayerPosition(currentPlayer);
-    } while (gameStatus == CONTINUE);
+        while (getchar() != '\n');
+        
+        askPlayerPosition();
+    } while (player1 == WON);
     
     
 }
@@ -50,27 +50,27 @@ void showGameDisplay(char a, char b, char c, char d, char e, char f, char g, cha
     printf("a%4c b%4c c%4c\nd%4c e%4c f%4c\ng%4c h%4c i%4c\n", a, b, c, d, e, f, g, h, i);
 }
 
-char askPlayerPosition(enum Status currentPlayer) {
+char askPlayerPosition() {
     char position;
 
     if (currentPlayer == P1) {
         printf("%s", "Player 1: ");
         position = getchar();
         //scanf("%c", &position);
-        insertSymbol(position, currentPlayer);
+        insertSymbol(position);
         currentPlayer = P2;
     } else {
         printf("%s", "Player 2: ");
         position = getchar();
         //scanf("%c", &position);
-        insertSymbol(position, currentPlayer);
+        insertSymbol(position);
         currentPlayer = P1;
     }
 
     return position;
 }
 
-void insertSymbol(char position, enum Status currentPlayer) {
+void insertSymbol(char position) {
     char symbol;
 
     if (currentPlayer == P1 ) {
@@ -120,5 +120,5 @@ void insertSymbol(char position, enum Status currentPlayer) {
 }
 
 enum Status checkPosition(char position) {
-    return CONTINUE;
+    return WON;
 }
