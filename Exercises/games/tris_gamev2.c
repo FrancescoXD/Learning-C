a34b6e103d7acbae8bacb0252cda993b5f216918
Francesco <28204054+FrancescoXD@users.noreply.github.com>
2020-07-03 23:43:34 +0200

    Update tris_gamev2.c

--- Exercises/games/tris_gamev2.c
+++ Exercises/games/tris_gamev2.c
@@ -3,67 +3,59 @@
 
 enum Player { P1, P2 };
 enum PlayerStatus { WIN, LOSE, TIE };
-enum Player currentPlayer = P1;
+int currentPlayer = 1;
 
 void showGameTable();
 void askPlayerPosition();
 void insertSymbol();
+enum PlayerStatus checkIfWon();
 
-char table[] = {'a', 'b', 'c',
-                'd', 'e', 'f',
-                'g', 'h', 'i'};
+char table[] = {' ', ' ', ' ',
+                ' ', ' ', ' ',
+                ' ', ' ', ' '};
 char position;
-int index;
 char symbol;
 
 int main() {
     showGameTable();
+    askPlayerPosition();
+
+    do {
+        while (getchar() != '\n');
+        showGameTable();
+        askPlayerPosition();
+    } while (1);
+    
 }
 
 void askPlayerPosition() {
-    if (currentPlayer == P1) {
+    if (currentPlayer % 2 == 1) {
         symbol = 'X';
+        printf("%s", "Player 1: ");
         position = getchar();
-        
+        insertSymbol();
     } else {
         symbol = 'O';
+        printf("%s", "Player 2: ");
         position = getchar();
+        insertSymbol();
     }
 }
 
 void insertSymbol() {
-    switch (position) {
-    case 'a':
-        index = 0;
-    case 'b':
-        index = 1;
-    case 'c':
-        index = 2;
-    case 'd':
-        index = 3;
-    case 'e':
-        index = 4;
-    case 'f':
-        index = 5;
-    case 'g':
-        index = 6;
-    case 'h':
-        index = 7;
-    case 'i':
-        index = 8;
-    case '\t':
-    case '\n':
-    case ' ':
-        break;
-    
-    default:
-        puts("Wrong position!");
-        break;
+    int index = position - 'a';
+    if (table[index] == ' ') {
+        table[index] = symbol;
+        ++currentPlayer;
+    } else {
+        puts("Slot not empty!");
     }
 }
 
 void showGameTable() {
     unsigned int cont = 0;
+    
+    printf("%4c%4c%4c\n%4c%4c%4c\n%4c%4c%4c\n\n", 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i');
 
     for (size_t i = 0; i < TABLE_SIZE; ++i) {
         printf("%4c", table[i]);
