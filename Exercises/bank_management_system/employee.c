5d3ea7920ea5ad70603b4848d5963554adb296b7
FrancescoXD <28204054+FrancescoXD@users.noreply.github.com>
2020-09-12 16:55:46 +0200

    Test

--- Exercises/bank_management_system/employee.c
+++ Exercises/bank_management_system/employee.c
@@ -12,29 +12,3 @@ void add_employee() {
     employee_list[0].name = "Giorgio";
     printf("%s", employee_list[0].name);
 }
-
-// Remove an employee
-
-int remove_employee(int array[], int n, int key) {
-    int pos = findEmployee(employee_list, n, key);
-
-    if (pos == -1) {
-        puts("Employee not found!");
-        return n;
-    }
-
-    for (size_t i = pos; i < n; ++i) {
-        array[i] = array[i + 1];
-    }
-
-    return n - 1;
-}
-
-int findEmployee(int array[], int n, int key) {
-    for (size_t i = 0; i < n; ++i) {
-        if (array[i] == key)
-            return i;
-    }
-
-    return n - 1;
-}
